5b8de56ac4f82ae2272e29eeb4494639bc9c9e41
193289revathi <193289revathi@gmail.com>
2025-09-01 12:52:39 +0530

    Fix stop.sh to prevent ApplicationStop failure

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 echo "Stopping existing Node app..."
-pkill -f "node index.js" || true
+# Stop node app if running, ignore error if not running
+if pgrep -f "node index.js" > /dev/null
+then
+  pkill -f "node index.js"
+  echo "Node app stopped."
+else
+  echo "No Node app running."
+fi
 
